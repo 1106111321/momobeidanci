b0a5bcab13f41be6185e13e488a990e3ca41cbd3
wangjiali123 <wangjiali123>
2024-01-17 23:01:03 +0800

    修改链接

---
+++ gitPushToGitHub.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /home/wwwroot/momobeidanci
+echo "1、拉取代码..."
+#git reset --hard origin/master
+#git clean -f
+git pull
+#git checkout master
+
+echo "2、推送gitHub..."
+for((i=1;i<=10;i++));do
+  git push gitHub_origin
+done
+
+echo "3、部署完成"
